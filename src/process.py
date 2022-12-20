60d6790992f7c18961599254f5be3ddcdf0a9393
JacqueWong <jacquewong@stu.jluzh.edu.cn>
2022-12-20 11:03:37 +0800

    Implement Process class and modify TODO

--- src/process.py
+++ src/process.py
@@ -11,97 +11,81 @@
 from lib import *
 
 
+# TODO All functions need to be tested, and the logic is not yet perfect.
+#  It also needs to be optimized, using the [map] function
 class Process:
     def __init__(self):
-        # base class load source
-        self.source = Source()
-        self.process = []
+        # base class load src
+        self.src = Source()
 
-
-# TODO many many many many many logical modules(all the 'pass') need to be done
-class Game(Process):
     def open_game(self):
-        full_mode()
-        temp = [
-            [self.source.get_source('game_icon'), 1],
-            [self.source.get_source('update_OK'), 0],
-            [self.source.get_source('start_page'), 0],
-            [self.source.get_source('collapse_drop_down_box'), 1]
-        ]
-        for i, j in temp:
-            print(i, j)
-
-
-class Gains(Process):
-    def gains(self):
-        pass
+        # full_mode()
+        temp = ['game_icon', 'update_OK', 'start_page', 'collapse_drop_down_box']
+        var = map(ctp, map(self.src.get(), temp), [1, 0, 0, 1])
+        for i in var:
+            print(i)
 
     def gains_crucible(self):
-        pass
+        print('function gains_crucible')
+        ctp(self.src.get('crucible'))
 
     def gains_hourglass(self):
-        pass
+        print('function gains_hourglass')
+        ctp(self.src.get('ok'))
 
     def gains_arena(self):
-        pass
+        print('function gains_arena')
+        ctp(self.src.get('arena_button'))
+        count = 3
+        while count:
+            ctp(self.src.get('reduce_times'))
+            ctp(self.src.get('ok'))
+            count = count - 1
+        ctp(self.src.get('daily_rewards'))
 
-
-class Venture(Process):
-    def __init__(self):
-        super().__init__()
-        # TODO write json file? Complete control process
-        # action list , about count/index/steps,click/drag button name/icon,times/length,range,mark
-        # self.action = [1, 'click', 'tavern_button', 1, [0, 0, 1, 1], 'Enter the tavern']
-        # maybe dict better than list
-        # self.action = {
-        #     'step': 1,
-        #     'event': 'click',
-        #     'target': 'tavern_button',
-        #     'parameter': 1,
-        #     'evade': 10,
-        #     'remarks': 'Enter the tavern'
-        # }
-        # self.action = {
-        #     'step': 666,
-        #     'event': 'drag',
-        #     'target': 'duration_button',
-        #     'parameter': 400,
-        #     'evade': 10,
-        #     'remarks': 'Set adventure time, 8 hours'
-        # }
+    def venture(self):
+        self.disassemble_equipment()
+        self.receive_proceeds()
+        self.start_venture()
 
     def start_venture(self):
-        pass
+        ctp(self.src.get('tavern_button'))
+        # count = 3
+        # while count:
+        #     count = count -1
 
     def receive_proceeds(self):
-        pass
+        ctp(self.src.get('venture_finish'))
+        count = 3
+        while count:
+            count = count - 1
+        ctp(self.src.get('receive'))
+        ctp(self.src.get('receive_and_leave'))
 
     def disassemble_equipment(self):
-        pass
-
-
-class Benefit(Process):
-    def times(self):
-        pass
+        ctp(self.src.get('warehouse_button'))
+        ctp(self.src.get('equip'))
+        ctp(self.src.get('Batch decomposition'))
+        ctp(self.src.get('decompose_purple_gear'))
+        ctp(self.src.get('ok'))
+        ctp(self.src.get('leave_4'))
 
     def manor_double_benefit(self):
-        pass
-
-
-class Church(Process):
-    def last_time(self):
-        pass
+        ctp(self.src.get('manor_button'))
+        count = 5
+        while count:
+            count = count - 1
+            ctp(self.src.get('double_benefits'))
+            ctp(self.src.get('ok'))
+        ctp(self.src.get('leave_2'))
 
     def church_personal_tasks(self):
-        pass
-
-
-class Market(Process):
-    def last_time(self):
-        pass
-
-    def next_time(self):
-        pass
+        ctp(self.src.get('parthenon'))
+        ctp(self.src.get('personal_tasks'))
+        count = 3
+        while count:
+            count = count - 1
+            ctp(self.src.get('ads_icon'))
 
     def market_automatic_purchases(self):
         pass
