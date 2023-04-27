4da81a2d70e89f98204612337f3f33e4d42bc9eb
JacqueWong <jacquewong@stu.jluzh.edu.cn>
2023-04-27 14:40:32 +0800

    Optimize log module logic

--- lib/log.py
+++ lib/log.py
@@ -12,17 +12,19 @@ from lib.config import Config
 
 
 def dlog(name):
-    if 'src' in os.path.abspath('.'):
+    if 'src' in os.path.abspath('.') or 'lib' in os.path.abspath('.'):
         os.chdir('./..')
-    # print(os.path.abspath('.'))
     level = Config().get_config(["log", "level"])
     log_dir = "Logs"
     date_format = "%Y-%m-%d"
-    log_format = '%(levelname)s \t:' \
-                 '%(asctime)s ' \
-                 '<%(filename)s>' \
-                 '[%(lineno)d] ' \
-                 '\t%(message)s'
+    if level == 10:
+        log_format = '%(levelname)s \t:' \
+                     '%(asctime)s ' \
+                     '<%(filename)s>' \
+                     '[%(lineno)d]' \
+                     ': %(message)s'
+    else:
+        log_format = '%(levelname)s : %(asctime)s : %(message)s'
 
     logger = logging.getLogger(name)
     logger.setLevel(level)
--- lib/test.py
+++ lib/test.py
@@ -4,16 +4,13 @@
 # @Author  : Jacque
 # @Mail    : Jacquewong1111@outlook.com
 
-
 import time
 
 from lib import Auto
-
 auto = Auto()
 start_time = time.perf_counter()
-print(auto.mtp("./../static/target.png", waiting=0))
+print(auto.mtp("static/target.png", waiting=0))
 end_time = time.perf_counter()
-
 elapsed_time = end_time - start_time
 
-print(f"函数执行时间: {elapsed_time:0.6f} s")  #
+print(f"函数执行时间: {elapsed_time:0.6f} s")
